fb9196094e02bd8fb1fb563196205aabe6be2585
Josh <g00409871@atu.ie>
2023-09-22 12:11:15 +0100

    first commit

---
+++ Exercise1.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+
+function SayHello(){
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function SayHi(){
+    console.log("Hi");
+}
+
+myFunction();
+
+const myNewFunction = function(){
+    console.log("Bonjur")
+}
+
+myNewFunction();
+
+
+const myComponent = ()=>{
+    console.log("Bye");
+}
+
+myComponent();
